290fc4eac5133efd3e4c82a483c8dce5b382d25c
Taiwo Akinnusoye <taiwo@kudan.eu>
2024-10-21 03:13:42 +0100

    add rabbit mq for services communication

--- apps/payments/src/main.ts
+++ apps/payments/src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     transport: Transport.RMQ,
     options: {
       urls: [configService.getOrThrow('RABBITMQ_URI')],
+      noAck: false,
       queue: 'payments',
     },
   });
